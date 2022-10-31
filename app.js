a4fb9b253751512d708e142d5a19dfc86b09ee6f
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-10-31 23:22:28 +0400

    finished the first 10 exercises in beginners challenges

---
+++ app.js
@@ -0,0 +1,95 @@
+//BEGINNER CHALLENGES
+
+// 1)ADDITION
+
+/* function addition(num1, num2) {
+    return num1 + num2
+    
+}
+
+
+console.log(addition(3, 5)); */
+/////////////////////////////
+// 2)convert hours into secons
+
+
+/* function hoursIntoSeconds(hour) {
+    return hour * 60 * 60;
+    return seconds //what we want to return
+}
+
+console.log(hoursIntoSeconds(7))
+ */
+////////////////////////////
+
+// 3) calculate the perimeter of a rectangle
+
+/* function calcPerimeter(length, width) {
+    return length * 2 + width * 2;
+}
+
+console.log(calcPerimeter(5, 10)) */
+////////////////////////// 
+// 4) calculate the area of a Triangle
+
+/* 
+function calcTriangleArea(base, height) {
+    return 0.5 * base * height;
+}
+
+console.log(calcTriangleArea(10, 10)) */
+
+
+////////////////////////
+
+// 5) extend a string
+
+/* function appendFrontend(word) {
+    return word + 'Frontend';
+}
+
+console.log(appendFrontend('Gurami')) */
+
+
+///////////////////////////
+
+// 6) return true if the sum is > 100
+
+/* function sumGreaterThan100(num1, num2) {
+    return num1 + num2 > 100;
+}
+
+console.log(sumGreaterThan100(10, 99))
+ */
+
+///////////////////////
+
+// 7) if less than or equal to 0 return true , 'else false'
+
+
+/* function lessThanOrEqualToZero(num) {
+    return num <= 0;
+}
+
+console.log(lessThanOrEqualToZero(2)) */
+
+
+//////////////////////////
+
+// 8) return opposite boolean
+
+/* function oppositeBoolean(boolean) {
+    return boolean != true
+}
+// or return !true; gives the same output
+console.log(oppositeBoolean(true)) */
+
+
+///////////////////////////
+
+// 9)if not 0 return true
+
+function isNotZero(num) {
+    return num !== 0;
+}
+console.log(isNotZero(0))
